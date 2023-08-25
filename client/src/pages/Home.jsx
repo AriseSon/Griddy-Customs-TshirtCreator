e142da6c9e27753a2cadd376264b78abbcadc3ff
Ricky Fort <codeworld85@gmail.com>
2023-08-25 18:14:32 -0500

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+import { CustomButton } from "../components";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./griddycustoms.png"
+              alt="logo"
+              className="w-12 h-12 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> CREATE{" "}
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-800 text-base">
+                Create your own customs for your brand and make it easy for you
+                to create your own customs.{" "}
+                <strong>Elevate Your Creativity</strong>
+                {""} and define your own style.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-lg text-gray-800"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
